5bfdf9243dcba0fb863b5467fdabb6d7a4b5dfd2
Chris Kelly <c-kelly@ti.com>
2009-09-11 13:42:40 -0500

    add calls to install system sounds in zoom2 product makefile

--- zoom2-generic.mk
+++ zoom2-generic.mk
@@ -5,6 +5,12 @@ PRODUCT_PACKAGES := \
 
 $(call inherit-product, build/target/product/generic.mk)
 
+SYS_SOUNDS_PATH := frameworks/base/data/sounds
+
+include $(SYS_SOUNDS_PATH)/OriginalAudio.mk
+
+include $(SYS_SOUNDS_PATH)/AudioPackage2.mk
+
 # Overrides
 PRODUCT_BRAND := zoom
 PRODUCT_NAME := zoom2
