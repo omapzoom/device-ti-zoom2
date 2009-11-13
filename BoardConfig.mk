4bf065bb3478fdcc6cd5b8f549b31c546fc3d865
Vishal Mahaveer <a0271468@ti.com>
2009-11-13 16:37:52 -0600

    WLAN - Eclair Adding support for TI WLAN

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,8 +18,7 @@ TARGET_NO_RADIOIMAGE := true
 # Wifi
 BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
 BOARD_WLAN_TI_STA_DK_ROOT   := system/wlan/ti/wilink_6_1
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/tiwlan_drv.ko"
-WIFI_DRIVER_MODULE_ARG      := "sdc_ctrl=3"
+WIFI_DRIVER_MODULE_PATH     := "/system/etc/wifi/tiwlan_drv.ko"
 WIFI_DRIVER_MODULE_NAME     := "tiwlan_drv"
 WIFI_FIRMWARE_LOADER        := "wlan_loader"
 
