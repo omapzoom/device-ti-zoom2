9a9828d036ad466e1fe5e4f16c087533740a1127
Daniel Charles <dcharles@ti.com>
2010-07-13 15:47:55 -0500

    PM/RM removed from system
    Multimedia OMX Policy Manager and OMX Resource Manager are removed from the
    system.  There's no need to try to lauch the daemons
    
    Also, removed the policy table references and its installation to the system
    
    Change-Id: I1fa52ae9f02b9ec23b8a45d2b2d2b79ae7f39a79

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -19,16 +19,12 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/TWL4030_Keypad.kl:system/usr/keylayout/TWL4030_Keypad.kl
 
 
-# cellular operators apn list, vold config, OMX policy table
+# cellular operators apn list, vold config, media_profiles.xml
 #
-ifeq ($(HARDWARE_OMX),true)
-TI_OMX_POLICY_MANAGER := hardware/ti/omx/system/src/openmax_il/omx_policy_manager/src/policytable.tbl:system/etc/policytable.tbl
-endif
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/vold.conf:system/etc/vold.conf \
         $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
-        $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
-	$(TI_OMX_POLICY_MANAGER)
+        $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml
 
 # keyboard maps
 #
