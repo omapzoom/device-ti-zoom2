d2a21ed393e380694c065ede02c7df34dcbab461
Praneeth Bajjuri <praneeth@ti.com>
2010-08-09 17:28:21 -0400

    Keypad layout and character map fix for 2.6.32 kernel on Zoom3
    Froyo checks for keypad dev name ending '-keypad'.Hence the naming
    convention has to be changed accordingly.
    
    This patch also wrong key character mapping for ; and + buttons
    on Zoom3.
    
    Remove unnecessary keycharactermap and layout files
    Change-Id: I94a8aabc0ad75204c8cc1349e9ae15c10e866185
    Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>
    
    Patch Set 9 -- fixed whitespace issues in twl4030-keypad.kl and
    AndroidBoard.mk file.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -10,33 +10,29 @@ endif
 file := $(INSTALLED_KERNEL_TARGET)
 ALL_PREBUILT += $(file)
 $(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-	$(transform-prebuilt-to-target)
+        $(transform-prebuilt-to-target)
 
 # keyboard layouts
 #
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/omap_twl4030keypad.kl:system/usr/keylayout/omap_twl4030keypad.kl \
-	$(LOCAL_PATH)/TWL4030_Keypad.kl:system/usr/keylayout/TWL4030_Keypad.kl
+        $(LOCAL_PATH)/twl4030-keypad.kl:system/usr/keylayout/twl4030-keypad.kl
 
 
 # cellular operators apn list, vold config, media_profiles.xml
 #
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/vold.conf:system/etc/vold.conf \
+        $(LOCAL_PATH)/vold.conf:system/etc/vold.conf \
         $(LOCAL_PATH)/vold.fstab:system/etc/vold.fstab \
         $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml
 
 # keyboard maps
 #
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := omap_twl4030keypad.kcm
-LOCAL_SRC_FILES := TWL4030_Keypad.kcm
-
+LOCAL_SRC_FILES := twl4030-keypad.kcm
 include $(BUILD_KEY_CHAR_MAP)
 
 # board specific init.rc
 #
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/init.omapzoom2.rc:root/init.omapzoom2.rc \
-	$(LOCAL_PATH)/env.txt:root/env.txt
-
+        $(LOCAL_PATH)/init.omapzoom2.rc:root/init.omapzoom2.rc \
+        $(LOCAL_PATH)/env.txt:root/env.txt
